d87edc7cc42054580987f516a5188538a4379ff1
Jeroen Kortekaas <studio@jeroenkortekaas.com>
2020-06-08 18:11:00 +0200

    Page and Project queries and templates added

---
+++ src/templates/BlogPost.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const BlogPostTemplate = ({ data }) => (
+  <Layout>
+    <SEO
+      title={data.wordpressWpProjects.title}
+      description={data.wordpressWpProjects.excerpt}
+    />
+    <h1>{data.wordpressWpProjects.title}</h1>
+    <div
+      style={{ marginTop: 20 }}
+      dangerouslySetInnerHTML={{ __html: data.wordpressWpProjects.content }}
+    />
+  </Layout>
+)
+export default BlogPostTemplate
+
+export const query = graphql`
+  query($id: Int!) {
+    wordpressWpProjects(wordpress_id: { eq: $id }) {
+      title
+      content
+    }
+  }
+`
