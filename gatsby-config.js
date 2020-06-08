0f1bd4335a19c56e7f827f52b637b277e5e0075d
Jeroen Kortekaas <studio@jeroenkortekaas.com>
2020-06-08 18:45:15 +0200

    Site title rendering

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Jeroen Kortekaas`,
+    description: `Jeroen Kortekaas creates sculptural collage and images dealing with motifs of animation and transportation.`,
+    author: `@luminc`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -9,6 +9,7 @@ const BlogPostTemplate = ({ data }) => (
       title={data.wordpressWpProjects.title}
       description={data.wordpressWpProjects.excerpt}
     />
+    <Link to="/">Back</Link>
     <h1>{data.wordpressWpProjects.title}</h1>
     <div
       style={{ marginTop: 20 }}
