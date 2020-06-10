2195052b0d8143c54277a081fe4cc5c7eaffd18b
Jeroen Kortekaas <studio@jeroenkortekaas.com>
2020-06-10 12:39:04 +0200

    Ico added

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: `Jeroen Kortekaas`,
     description: `Jeroen Kortekaas creates sculptural collage and images dealing with motifs of animation and transportation.`,
     author: `@luminc`,
+    siteUrl: `https://www.jeroenkortekaas.com/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ico32.png`, // This path is relative to the root of the site.
       },
     },
     {
