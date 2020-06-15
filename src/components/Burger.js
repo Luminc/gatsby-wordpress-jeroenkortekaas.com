c34f5d8299a9ed394bcfc29c9407e14e970478d5
Jeroen Kortekaas <studio@jeroenkortekaas.com>
2020-06-15 18:59:45 +0200

    Added burger

---
+++ src/components/Burger.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Burger = () => {
+  return (
+    <div class="burger">
+      <div class="line1"></div>
+      <div class="line2"></div>
+      <div class="line3"></div>
+    </div>
+  )
+}
+
+export default Burger
